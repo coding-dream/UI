419935913e4838193581ba2d8adee129f793772a
wangli <352085768@qq.com>
2016-12-21 17:26:34 +0800

    first commit QQ 身边的人

---
+++ gallery/src/main/java/com/ruoxu/gallery/Item.java
@@ -0,0 +1,42 @@
+package com.ruoxu.gallery;
+
+import java.io.Serializable;
+
+public class Item implements Serializable{
+    private int position;
+    private int img;
+    private String name;
+    private boolean isSelected;
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
